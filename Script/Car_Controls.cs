84d99271bf58bfa797df1d536bf0d6ed8869ea6d
Pnevsk123 <vlad.007.p@gmail.com>
2017-08-22 15:27:10 +0300

    first commit

---
+++ Script/Car_Controls.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car_Controls : MonoBehaviour {
+    public float Speed;
+    private Rigidbody2D _rb;
+
+
+
+    // Use this for initialization
+    void Start () {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+
+        Vector3 direction = new Vector3(moveHorizontal, 0f, 0f);
+        transform.position += direction * Speed * Time.deltaTime;
+
+    }
+}
---
+++ Script/Car_Movement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car_Movement : MonoBehaviour {
+  public float speed = 15f;
+	// Use this for initialization
+	void Start () {
+        
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * speed, ForceMode2D.Force);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
